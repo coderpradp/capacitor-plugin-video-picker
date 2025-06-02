e6a349c591de0ddde1a4b0836723ca9780aadc9f
Pradip Khadka <20035221+coderpradp@users.noreply.github.com>
2025-06-03 00:59:08 +0545

    capacitor 7 support

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { registerPlugin } from '@capacitor/core';
 import type { VideoPickerPlugin } from './definitions';
 
 const VideoPicker = registerPlugin<VideoPickerPlugin>('VideoPicker', {
-  web: () => import('./web').then(m => new m.VideoPickerWeb()),
+  web: () => import('./web').then((m) => new m.VideoPickerWeb()),
 });
 
 export * from './definitions';
